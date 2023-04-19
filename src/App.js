a2293b9d1c389f7f5aaf8326824c2aa573a60571
Sakal05 <samnang.sakal@gmail.com>
2023-04-19 12:10:49 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// Import everything needed to use the `useQuery` hook
+import { useQuery, gql } from '@apollo/client';
 
-function App() {
+const GET_PROPOSALS = gql`
+  query ProposalEvent {
+      proposalEvents {
+        id
+        owner
+        title
+        description
+        
+      }
+    
+  }
+`;
+
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_PROPOSALS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  console.log(data.proposalEvents);
+  // data.proposalEvents.map((obj) => {
+  //   console.log(`Owner: ${obj.owner}`)
+  // })
+  return data.proposalEvents.map((obj) => (
+    <div key={obj.id}>
+      <h3>Title: {obj.title}</h3>
+      <h3>Owner: {obj.owner}</h3>
+      <br />
+      <b>Description:</b>
+      <p>{obj.description}</p>
+      <br />
+    </div>
+  ));
+}
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <br/>
+      <DisplayLocations />
     </div>
   );
 }
-
-export default App;
